df88a68f6bc2c637f92ac6b835c9d6e5821e7f6e
Wendy k. Yeung <96136078+wendy174@users.noreply.github.com>
2025-07-11 17:05:46 -0400

    testing automation

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -5,6 +5,7 @@ import yeschefImg from "@/assets/yeschef.jpg";
 import comicImg from "@/assets/comic.jpg";
 import groceryAgentImg from "@/assets/ai_agent.png"; 
 import chinatownImg from "@/assets/chinatown.png"; 
+import projectAssistantImg from "@/assets/projectAssistant.png";
 
 export type Project = {
   title: string;
@@ -17,6 +18,16 @@ export type Project = {
 };
 
 export const projects: Project[] = [
+  {
+    title: "",
+    description: "",
+    image: projectAssistantImg,
+    bullets: [],
+    technologies: [],
+    githubUrl: "",
+    demoUrl: "",
+  },
+
 
   { title: "AI Agent for Weekly Grocery Deals",
     description:
